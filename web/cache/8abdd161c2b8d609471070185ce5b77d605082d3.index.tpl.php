6f6a1c79e4b936c051b0faad0e49b360382b2503
Ivan Dublianski <ivan.dublianski@gmail.com>
2015-06-28 17:35:44 +0200

    test

--- web/cache/8abdd161c2b8d609471070185ce5b77d605082d3.index.tpl.php
+++ web/cache/8abdd161c2b8d609471070185ce5b77d605082d3.index.tpl.php
@@ -30,10 +30,10 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'nocache_hash' => '2023654fb8b2899da52-60148829',
   'cache_lifetime' => 1,
   'version' => 'Smarty-3.1.21-dev',
-  'unifunc' => 'content_5569d5c1b070a6_55389775',
+  'unifunc' => 'content_557d754ad07cb3_51206963',
   'has_nocache_code' => false,
 ),true); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_5569d5c1b070a6_55389775')) {function content_5569d5c1b070a6_55389775($_smarty_tpl) {?><!DOCTYPE html>
+<?php if ($_valid && !is_callable('content_557d754ad07cb3_51206963')) {function content_557d754ad07cb3_51206963($_smarty_tpl) {?><!DOCTYPE html>
 <html>
 <head>
         <title>V Desktop start page</title>
